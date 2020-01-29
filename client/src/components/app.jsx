a935e39a35aab60fd9c301c4be917dcfcf8321b7
Ben Kidder <r.ben.kidder@gmail.com>
2020-01-29 10:59:44 -0800

    Make Get query on render and create class component

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+const React = require('react');
+const $ = require('jquery');
 
-function App() {
-  return (
-    <div>
-      <h1>Deals/Checkout</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Package: [],
+      deals: [],
+    };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:3000/api/checkout/1',
+      success: (packageDeal) => {
+        const Package = packageDeal.packageData;
+        const deals = packageDeal.dealData.flat(1);
+        this.setState({ Package, deals });
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Deals/Checkout</h1>
+        <p>{ JSON.stringify(this.state.Package) }</p>
+        <p>{ JSON.stringify(this.state.deals) }</p>
+      </div>
+    );
+  }
 }
 
 export default App;
