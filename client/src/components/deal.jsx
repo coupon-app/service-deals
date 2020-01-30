f966ee5ec13759e653096861f17a8400bec96a62
Ben Kidder <r.ben.kidder@gmail.com>
2020-01-29 16:50:54 -0800

    Create files for future basic components

---
+++ client/src/components/deal.jsx
@@ -0,0 +1,32 @@
+// const React = require('react');
+// // const styled = require('styled-components');
+
+// const Deal = (props) => (
+//   props.deals.map((deal) => (
+//     <label>
+//       <input
+//         id={deal.id}
+//         type="radio"
+//         name="packageDeals"
+//         value={deal.title}
+//         onChange={props.onChange}
+//         checked={props.selectedOption === `${deal.title}`}
+//       />
+//       {deal.title}
+//       <p>
+//         <span>
+//           {'$' + deal.msrp}
+//         </span>
+//         <span>
+//           {' $' + deal.salePrice}
+//         </span>
+//       </p>
+//       <p>
+//         <span>{deal.bought + '+ bought '}</span>
+//         <span>{deal.salePercent + '% OFF'}</span>
+//       </p>
+//     </label>
+//   ))
+// );
+
+// module.exports = { Deal };
