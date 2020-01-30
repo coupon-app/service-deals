532644bdda87b0ad01b7cbab3e4979d062127beb
Ben Kidder <r.ben.kidder@gmail.com>
2020-01-29 16:48:49 -0800

    Complete API to save to state and refactor module render content

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const $ = require('jquery');
+const { Deals } = require('./deals.jsx');
 
 class App extends React.Component {
   constructor(props) {
@@ -7,27 +8,44 @@ class App extends React.Component {
     this.state = {
       Package: [],
       deals: [],
+      selectedOption: '',
     };
+    this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
     $.ajax({
       method: 'GET',
-      url: 'http://localhost:3000/api/checkout/1',
+      url: 'http://localhost:3000/api/checkout/4',
       success: (packageDeal) => {
-        const Package = packageDeal.packageData;
-        const deals = packageDeal.dealData.flat(1);
-        this.setState({ Package, deals });
+        const Package = packageDeal.packageData[0];
+        const deals = packageDeal.dealData[0];
+        this.setState({ Package, deals, selectedOption: deals[0].title });
       },
     });
   }
 
+  onChange(e) {
+    this.setState({
+      selectedOption: `${e.target.value}`,
+    });
+  }
+
   render() {
+    const { Package, deals, selectedOption } = this.state;
     return (
       <div>
-        <h1>Deals/Checkout</h1>
-        <p>{ JSON.stringify(this.state.Package) }</p>
-        <p>{ JSON.stringify(this.state.deals) }</p>
+        <form>
+          <Deals
+            onChange={this.onChange}
+            selectedOption={selectedOption}
+            deals={deals}
+            key={Package.id}
+          />
+          <input type="submit" value="Buy" />
+          <br />
+          <input type="submit" value="Give as a Gift" />
+        </form>
       </div>
     );
   }
