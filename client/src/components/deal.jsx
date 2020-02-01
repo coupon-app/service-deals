08359d1203183956533cf77c61936b19f9e86be4
Ben Kidder <r.ben.kidder@gmail.com>
2020-01-31 23:02:46 -0800

    Remove unnecessary component and refactor styling for extra sale

--- client/src/components/deal.jsx
+++ client/src/components/deal.jsx
@@ -1,120 +1,105 @@
 import React from 'react';
 import styled from 'styled-components';
-import Price from './price';
 
-const Deal = ({ deal, onChange, selectedOption, onSale }) => {
+const Deal = ({ deal, onChange, selectedOption, onSale, dealEnd }) => {
   return deal.soldOut ? (
-  <LabelWrap soldOut={deal.soldOut}>
-    <DealTitleWrap>{deal.title}</DealTitleWrap>
-    <SoldOutRadio />
-    <InputWrap
-      id={deal.id}
-      type="radio"
-      name="packageDeals"
-      value={deal.title}
-      onChange={onChange}
-    />
-    <PriceWrap>
-      <MSRPWrap>
-        <Price value={deal.msrp} />
-      </MSRPWrap>
-    </PriceWrap>
-    <BottomMetaWrap>
-      <BoughtWrap>{deal.bought + '+ bought'}</BoughtWrap>
-      <SoldOutWrap>Sold Out</SoldOutWrap>
-    </BottomMetaWrap>
-  </LabelWrap>
+    <LabelWrap soldOut={deal.soldOut}>
+      <TitleWrap>
+        <SoldOutRadio />
+        <DealTitleWrap>{deal.title}</DealTitleWrap>
+        <InputWrap
+          id={deal.id}
+          type="radio"
+          name="packageDeals"
+          value={deal.title}
+        />
+      </TitleWrap>
+      <MSRPWrap>{'$' + Math.trunc(deal.msrp)}</MSRPWrap>
+      <BottomMetaWrap buys={deal.bought} soldOut={deal.soldOut}>
+        <BoughtWrap>{deal.allowed + '+ bought'}</BoughtWrap>
+        <SoldOutWrap>Sold Out</SoldOutWrap>
+      </BottomMetaWrap>
+    </LabelWrap>
   ) : (
     <LabelWrap>
-      <DealTitleWrap>{deal.title}</DealTitleWrap>
-      <CustomRadio checked={selectedOption === `${deal.title}`} />
-      <InputWrap
-        id={deal.id}
-        type="radio"
-        name="packageDeals"
-        value={deal.title}
-        onChange={onChange}
-      />
+      <TitleWrap>
+        <CustomRadio checked={selectedOption === `${deal.title}`} />
+        <DealTitleWrap>{deal.title}</DealTitleWrap>
+        <InputWrap
+          id={deal.id}
+          type="radio"
+          name="packageDeals"
+          value={deal.title}
+          onChange={onChange}
+        />
+      </TitleWrap>
       {onSale ? (
-        <PriceWrap>
-          <MSRPWrap>
-            <Price value={deal.msrp} />
-          </MSRPWrap>
-          <SaleWrap onSale={onSale}>
-            <Price value={deal.salePrice} />
-          </SaleWrap>
-          <br />
-          <EPWrap>
-            <Price value={deal.extraPrice} />
-          </EPWrap>
-        </PriceWrap>
-
+        <div>
+          <PriceWrap>
+            <MSRPWrap>{'$' + Math.trunc(deal.msrp)}</MSRPWrap>
+            <SaleWrap onSale={onSale}>{'$' + Math.trunc(deal.salePrice)}</SaleWrap>
+          </PriceWrap>
+          <PriceWrap>
+            <PriceDiffWrap>{'Extra $' + Math.trunc(deal.salePrice - deal.extraPrice) + ' Off Ends ' + dealEnd[6] + '/' + dealEnd[9]}</PriceDiffWrap>
+            <ExSaleWrap>{'$' + Math.trunc(deal.extraPrice)}</ExSaleWrap>
+          </PriceWrap>
+        </div>
       ) : (
         <PriceWrap>
-          <MSRPWrap>
-            <Price value={deal.msrp} />
-          </MSRPWrap>
-          <SaleWrap>
-            <Price value={deal.salePrice} />
-          </SaleWrap>
+          <MSRPWrap>{'$' + Math.trunc(deal.msrp)}</MSRPWrap>
+          <SaleWrap>{'$' + Math.trunc(deal.salePrice)}</SaleWrap>
         </PriceWrap>
       )}
-      <BottomMetaWrap>
-        <BoughtWrap>{deal.bought + '+ bought'}</BoughtWrap>
+      <BottomMetaWrap buys={deal.bought}>
+        {deal.bought ? <BoughtWrap>{deal.bought + '+ bought'}</BoughtWrap> : null}
         <PercentWrap onSale={onSale}>{onSale ? deal.extraPercent + '% OFF' : deal.salePercent + '% OFF'}</PercentWrap>
       </BottomMetaWrap>
-      <br />
     </LabelWrap>
   );
 };
 
 const LabelWrap = styled.label`
-  width: 310px;
-  height: 100px;
-  position: relative;
-  display: block;
-  padding-left: 30px;
-  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  height: auto;
+  min-height: 85px;
+  margin-bottom: 4px;
   border-bottom: 1px solid #e6e7e8;
-  cursor: ${props => props.soldOut ? "default" : "pointer"};
+  cursor: ${(props) => (props.soldOut ? 'default' : 'pointer')};
+  font-family: Open Sans,OpenSans,system,-apple-system,BlinkMacSystemFont,Roboto,Arial,FreeSans,sans-serif;
 `;
 
-const DealWrap = styled.div`
-  font-family: Open Sans,OpenSans,system,-apple-system,BlinkMacSystemFont,Roboto,Arial,FreeSans,sans-serif;
-  position: relative;
-  top: 5px;
-  left: 0px;
+const TitleWrap = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: flex-start;
-  align-content: stretch;
-  color: black;
+  align-items: flex-start;
+  padding-top: 5px;
+
+`;
+
+const DealTitleWrap = styled.div`
+  flex-wrap: wrap;
   font-size: 15px;
   font-weight: 430;
   letter-spacing: 0.05rem;
-  padding: 2px;
+  margin-left: 10px;
 `;
 
-const DealTitleWrap = styled(DealWrap)`
-  top: 0px;
-  left: 2px;
-`;
 
 const InputWrap = styled.input`
+  position: absolute;
   opacity: 0;
-  width: 0;
-  height: 0;
-  position: relative;
 `;
 
 const CustomRadio = styled.span`
   width: 17px;
   height: 17px;
-  position: absolute;
-  top: 10px;
+  position: relative;
+  top: 9px;
   left: 0px;
   background: white;
-  border: ${props => props.checked ? "2px solid #0076d6" : "2px solid #d3d3d3"};
+  border: ${(props) => (props.checked ? '2px solid #0076d6' : '2px solid #d3d3d3')};
   border-radius: 50%;
   &::after {
     content: '';
@@ -125,101 +110,94 @@ const CustomRadio = styled.span`
     left: 3px;
     background: #0076d6;
     border-radius: 50%;
-    display: ${props => props.checked ? "block" : "none"};
+    display: ${(props) => (props.checked ? 'block' : 'none')};
   }
 `;
 
 const SoldOutRadio = styled.span`
   width: 17px;
   height: 17px;
-  position: absolute;
-  top: 10px;
+  position: relative;
+  top: 9px;
   left: 0px;
   background: #f7f7f7;
   border: 2px solid #d3d3d3;
   border-radius: 50%;
 `;
 
-const PriceWrap = styled(DealWrap)`
-  width: auto;
-  height: auto;
+//                        Price                       //
+
+const PriceWrap = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
-  align-items: baseline;
-  font-size: 17px;
-  font-weight: 400;
-  text-align: center;
-  letter-spacing: 0;
-  padding: 0px;
-  padding-top: 3px;
-  margin-bottom: 1px;
+  align-items: flex-end;
+  font-size: 18px;
+  font-weight: 430;
+  letter-spacing: 0.03rem;
 `;
 
 const MSRPWrap = styled(PriceWrap)`
-  width: 22%;
-  font-size: 15px;
-  font-weight: 300;
   color: gray;
+  font-weight: 300;
   text-decoration: line-through;
-  letter-spacing: 0.03rem;
-  padding: 0px;
 `;
 
 const SaleWrap = styled(PriceWrap)`
-  width: auto;
   font-weight: 430;
   color: rgb(52, 135, 0);
-  text-decoration: ${props => props.onSale ? "line-through" : "initial"};
-  letter-spacing: 0;
-  padding: 0px;
-  padding-left: 4px;
+  text-decoration: ${(props) => (props.onSale ? 'line-through' : 'initial')};
+  margin-left: 5px;
 `;
 
+//                      Middle                         //
 
-const EPWrap = styled(PriceWrap)`
-  width: 22%;
-  color: rgb(255, 0, 0);
-  letter-spacing: 0;
-  padding: 0px;
+const PriceDiffWrap = styled.div`
+  font-size: 12px;
+  font-weight: 455;
+  color: #F0001E;
+  background-color: #FFFFFF;
 `;
 
-const BottomMetaWrap = styled(DealWrap)`
-  justify-content: space-between;
-  letter-spacing: 0;
-  padding: 0px;
+const ExSaleWrap = styled.div`
+  color: #F0001E;
+  text-decoration: ${(props) => (props.onSale ? 'line-through' : 'initial')};
+  margin-left: 5px;
 `;
 
-const BoughtWrap = styled(DealWrap)`
-  top: 14px;
-  left: 2px;
+//                       Bottom                        //
+
+const BottomMetaWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: ${(props) => (props.buys ? 'space-between' : 'flex-end')};
+  align-items: baseline;
+  margin-top: ${(props) => (props.soldOut ? '8px' : '4px')};
+  margin-bottom: 5px;
+`;
+
+const BoughtWrap = styled.div`
   font-size: 12px;
   font-weight: 300;
   color: gray;
-  letter-spacing: 0px;
-  padding: 0px;
-  margin: 0px;
+  margin-left: 30px
 `;
 
-const PercentWrap = styled(DealWrap)`
-  width: 50px;
-  height: 12px;
-  display: inline-block;
+const PercentWrap = styled.div`
   font-size: 12px;
-  font-weight: 440;
-  text-align: center;
-  letter-spacing: 0;
-  color: ${props => props.onSale ? "rgb(255, 10, 0)" : "rgb(52, 135, 0)"};
-  background-color: ${props => props.onSale ? "rgb(255, 210, 210)" : "rgb(234, 252, 222)"};
-  padding: 4px 8px 8px 8px;
-  margin-top: 2px;
+  font-weight: 455;
+  color: ${(props) => (props.onSale ? '#F0001E' : '#40A500')};
+  background-color: ${(props) => (props.onSale ? '#FFF5F5' : '#EAFCDE')};
+  padding: 3px 10px;
+  margin-bottom: 5px;
   border-radius: 5px;
 `;
 
-const SoldOutWrap = styled(DealWrap)`
-  color: rgb(230, 0, 0);
-  font-weight: 475;
+const SoldOutWrap = styled.div`
+  color: #F0001E;
+  font-weight: 470;
   font-size: 18px;
+  letter-spacing: 0.05rem;
 `;
 
 
